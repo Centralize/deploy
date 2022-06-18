cee73ca46153ea69f74ef8d369f47e663e527f04
Mikkel Kaas <mkaas@openlab.dk>
2022-06-18 22:07:04 +0200

    Bugfix: syslog fixed

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ import sys
 import syslog
 
 
-syslog.syslog('INFO','Deployment started.')
+syslog.syslog('Deployment started.')
 
 # Prepare apt and system.
 os.system('apt update; apt upgrade -y')
@@ -15,6 +15,6 @@ mod = sys.argv[1]
 if(sys.argv[1] is not None):
     subprocess.call(f"{mod}/run.py")
 else:
-    syslog.syslog('ERROR','Missing module.')
+    syslog.syslog(syslog.LOG_CRIT, "Fatal Error: Missing module.")
 
 exit(0)
